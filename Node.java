3ad54edbe8be1c03485693f8898f92ddcc41fe4f
Na Li <nli@albany.edu>
2017-03-03 17:19:34 -0500

    Declare the constructors of Node class

--- Node.java
+++ Node.java
@@ -3,4 +3,16 @@ public class Node{
   Node nodes;
   Node next;
   Node prev;
+  public Node(String name){
+    this.next = null;
+    this.prev = null;
+    this.nodes = null;
+    this.str = name;
+  }
+  public Node(String name, Node node){
+    this.nodes = node;
+    this.str = name;
+    this.prev = null;
+    this.next = null;
+  }
 }
