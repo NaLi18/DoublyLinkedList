4fc6bdaa99520f2c3848610433eb262a2522ed40
Na Li <nli@albany.edu>
2017-03-11 17:10:45 -0500

    revise the constructor of Node class

--- Node.java
+++ Node.java
@@ -1,17 +1,14 @@
 public class Node{
   String str;
-  Node nodes;
   Node next;
   Node prev;
   public Node(String name){
     this.next = null;
     this.prev = null;
-    this.nodes = null;
     this.str = name;
   }
-  public Node(String name, Node node){
-    this.nodes = node;
-    this.str = name;
+  public Node(){
+    this.str = null;
     this.prev = null;
     this.next = null;
   }
