9c20107a261a85c2f060b7f790bc4a79efda0c2a
Na Li <nli@albany.edu>
2017-03-14 19:20:03 -0400

    Test the overloading constructor and the print method.

--- Demo.java
+++ Demo.java
@@ -1,8 +1,15 @@
 public class Demo{
   public static void main(String[] args){
+    /*
     //test the default constructor of Node class.
     Node myNode = new Node();
     System.out.println(myNode.getValue());
     System.out.println(myNode.getNext());
+    */
+    //test the constructor and print method 
+    Node myOtherNode = new Node("Frank");
+    System.out.println(myOtherNode.getValue());
+    System.out.println(myOtherNode.getNext());
+    myOtherNode.print();
   }
 }
